b916b73c36f1847d2e198682c9634764e0c9f447
bobrdobr1z <bobrdobr112zxc@bk.ru>
2026-02-16 21:19:14 +0300

    site

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+def root():
+    return FileResponse("public/index.html")
